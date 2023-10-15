9598e454e6d1c53d18f02ccb62a8ef6d7cb3913d
Saket Kothari <saketkothari722@gmail.com>
2023-10-15 13:16:32 +0530

    showing the search results on UI

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
   return (
     <div
       className={`w-[240px] md:block overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
-        mobileMenu ? 'translate-x-0' : ''
+        mobileMenu ? 'translate-x-0' : 'translate-x-[-240px]'
       }`}
     >
       <div className="flex px-5 flex-col">
